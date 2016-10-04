04124a58e484a15ef65d41460d6571d28a23dde8
Giorgio Mazzei <giorgio.mazzei@outlook.com>
2016-10-04 11:49:11 +0100

    minor changes

--- src/app/directives/blur.directive.ts
+++ src/app/directives/blur.directive.ts
@@ -5,19 +5,6 @@ import { Directive, ElementRef, HostListener, Input,trigger,
   animate  } from '@angular/core';
 @Directive({
   selector: '[myBlur]',
-  // animations: [
-  //       trigger('toBlur', [
-  //           state('inactive', style({
-  //               "-webkit-filter": 'blur(0px)',
-  //               filter: 'blur(0px)'
-  //           })),
-  //           state('active', style({
-  //               "-webkit-filter": 'blur(10px)',
-  //               filter: 'blur(10px)'
-  //           })),
-  //           transition('inactive => active', animate('500ms ease-in')),
-  //           transition('active => inactive', animate('500ms ease-out'))
-  //       ])]
 })
 export class BlurDirective {
   private el: HTMLElement;
