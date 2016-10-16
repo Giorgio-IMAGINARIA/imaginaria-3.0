e85d0583668687aaec9222edc9e2023da562f67a
Giorgio Mazzei <giorgio.mazzei@imaginaria.com>
2016-10-17 00:00:12 +0200

    contact page fixed

--- src/app/components/contact.component.ts
+++ src/app/components/contact.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+//Services
+import { BlurService } from '../services/blur.service';
 
 @Component({
   selector: 'my-contact',
   templateUrl: '../templates/contact.component.html',
-  styleUrls: ['../styles/contact.component.css']
+  styleUrls: ['../styles/contact.component.css'],
+  animations: [
+        trigger('toBlur', [
+            state('inactive', style({
+                "-webkit-filter": 'blur(0px)',
+                filter: 'blur(0px)'
+            })),
+            state('active', style({
+                "-webkit-filter": 'blur(10px)',
+                filter: 'blur(10px)'
+            })),
+            transition('inactive => active', animate('500ms ease-in')),
+            transition('active => inactive', animate('500ms ease-out'))
+        ])]
 })
 export class ContactComponent implements OnInit {
+     private blurStateString: string;
 
-  constructor() {
+  constructor(private blurService: BlurService) {
+         this.blurStateString = 'inactive';
   }
 
   ngOnInit() {
-   
+           this.checkBlurService();
   }
-
-  goBack() {
-  }
+  private checkBlurService(): void {
+        this.blurService.activeBlurStateObservable.subscribe(
+            response => response ? this.blurStateString = 'active' : this.blurStateString = 'inactive',
+            error => console.log('Error! Description: ' + error)
+        );
+    }
 }
